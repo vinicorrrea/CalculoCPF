2b4cfe1e79a18745244635b1f37945835454f5e6
Vinicius Corrêa <vinicius-correa2007@bol.com.br>
2018-04-24 11:20:57 -0300

    Estrutura e Variáveis

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculoCPF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpfCliente;
+            string cpf9;
+            string cpf10;
+            int contagem1=10;
+            int contagem2=11;
+            int resto=0;
+            int rs1=0;
+            int rs2=0;
+            
+        }
+    }
+}
